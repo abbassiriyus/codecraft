8254dfbca446d3487467e9438f91fdb7a9a2de85
Nigoraaa <94783351+Nigoraaa@users.noreply.github.com>
2021-12-27 12:14:08 +0300

    tonka
    chirigani

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
    <div>
      <Dashboard/>
-     <Nash_prinsip/>
+    
     </div>
   );
 }
--- src/js/Nash_prinsip.js
+++ src/js/Nash_prinsip.js
@@ -4,6 +4,12 @@ import imgs from '../img/child.jpg';
 import child from '../img/child.png';
 import rightImg from '../img/1111.png';
 import gril from '../img/kk.jpg';
+import image1 from '../img/o.png';
+import image2 from '../img/image2.png';
+import image3 from '../img/kom.png';
+import image4 from '../img/image4.png';
+import image5 from '../img/glob.png';
+import image6 from '../img/image6.png';
 import { Container,Col,Row} from 'react-bootstrap';
 import styles from '../css/nashPrinsip.module.css';
 
@@ -12,11 +18,11 @@ export default class Nash_prinsip extends React.Component {
         return (
             <div>
             <div className={styles.main}>
-            <Container>
+            <div className={styles.boxNash}>
                 <div className={styles.mainBox}>
                     <Row>
                         <h2 className={styles.title}>Наши принципы <span>преподавания</span></h2>
-                    <Col sm={12} md={12} lg={6} xxl={6}>
+                    <Col sm={12} md={12} md={{order: 1}} lg={6} xxl={6}>
                         <div className={styles.text}>
                         <p>Большая часть нашего учебного времени посвящена получению практического опыта при работе над проектами.</p>
                          <p>Работа над проектами побуждает наших студентов выражать себя с помощью кода.</p>
@@ -30,14 +36,12 @@ export default class Nash_prinsip extends React.Component {
                         </div>
                          </Col>
 
-                         <Col sm={12} md={12} lg={6} xxl={6}>
+                         <Col sm={12} md={12} md={12}  lg={6} lg={{order: 2}} xxl={6}>
                           <img className={styles.img} src={imgs} alt=" "/>
-                          <img className={styles.imgs} src={gril}  alt=" "/>
+                          <img className={styles.imgs} src={gril}   alt=" "/>
                          </Col>
                     </Row>
-                    </div>
-              
-                       <Row>
+                    <Row>
                            <Col sm={12} md={6} lg={6}>
                            <div className={styles.leftCard}>
                                <img src={child}  alt=" " />
@@ -54,9 +58,63 @@ export default class Nash_prinsip extends React.Component {
                            </div>
                            </Col>
                        </Row>
-                  
-                </Container>
+                    </div>
+            
+                </div>
             </div>
+
+
+            <div className={styles.codecrafMain}>
+             <h2 className={styles.title}>Почему выбирают <span>codecraft</span></h2>
+             <div className={styles.codecraf }>
+                 <div className={styles.boxNash}>
+                 <Row>
+                     <Col sm={12} md={6} lg={4}>
+                     <div >
+                         <img src={image1} />
+                         <h4>Самые клёвые инструкторы</h4>
+                         <p>Инструкторы Codecraft - молодые, энергичные программисты и айтишники, которые с легкостью найдут общий язык с детьми и помогут им понять даже самые сложные нюансы программирования</p>
+                     </div>
+                     </Col>
+
+                     <Col sm={12} md={6} lg={4}>
+                         <img src={image2}/>
+                         <h4>Индивидуальный подход</h4>
+                         <p>Мы умеем находить индивидуальный подход ко всем нашим ученикам, учитывая их разные характерные особенности</p>
+                     </Col>
+
+                    <Col sm={12} md={6} lg={4}> 
+                    <img src={image3}/>
+                    <h4>Современные технологии</h4>
+                    <p>Мы всегда следим за актуальностью наших учебных программ, следуя всевозможным современным тенденциям</p>
+                    </Col>
+         
+                      <Col  sm={12} md={6} lg={4}>
+                          <div>
+                              <img src={image4} />
+                              <h4>Удобный график занятий</h4>
+                              <p>Мы строим индивидуальный график занятий для каждого ученика, поэтому заниматься можно в любое удобное для Вас время</p>
+                          </div>
+                      </Col>
+                              <Col sm={12} md={6} lg={4}>
+                                  <div>
+                                      <img src={image5} />
+                                      <h4>Обучение из любой точки мира</h4>
+                                      <p>Благодаря дистанционной форме обучения, Вам не приходиться тратить время на дорогу, заниматься можно в любом удобном месте</p>
+                                  </div>
+                              </Col>
+                              <Col sm={12} md={6} lg={4}> 
+                              <div>
+                                  <img src={image6} />
+                                  <h4>Сертификат по окончанию</h4>
+                                  <p>Все ребята, при успешном завершении курса, получают от нашей школы подтверждающий сертификат с выпиской про оценки</p>
+                              </div>
+                              </Col>
+                      </Row>
+             </div>
+             </div>
+            </div>
+
         </div>
         )
     }
