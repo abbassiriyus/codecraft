c6a05e26ba1ee5aad5db405dc9da7c444a31bebc
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-12-28 09:30:00 +0500

    yh
    ghjg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Nash_prinsip from '../src/js/Nash_prinsip';
+import Nashprinsip from '../src/js/Nash_prinsip';
 import Dashboard from '../src/js/Dashboard';
 import Footere from '../src/js/footer'
 function App() {
   return (
    <div>
      <Dashboard/>
-     <Nash_prinsip/>
+     <Nashprinsip/>
      <Footere/>
     </div>
   );
