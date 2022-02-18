e6fef48aabc7547f92291241b9d7bc6de10775c2
abbas <siriyusabbos@gmail.com>
2022-02-18 21:55:38 +0500

    Revert "Revert "comments""
    This reverts commit 55b63eb6555ca6a753bbea8949993aebdfe2fe6e.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 
 import Menu1 from './Admin/js/Menu1';
 
-
+import Navbarr from './js/Navbarr'
 export default class App extends Component {
   render() {
     return <div>
-     <Menu1 />
-    </div>
-  
-    
+      <Menu1/>
+     {/* <Navbarr/> */}
+    </div>;
   }
 }
