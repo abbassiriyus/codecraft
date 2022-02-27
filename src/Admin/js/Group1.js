2243d7fa6c819ae9cd4ebf68bcd6e4e41c4c9ac6
abbas <86982819+abbassiriyus@users.noreply.github.com>
2022-02-27 15:24:09 +0500

    rr
    rrr

--- src/Admin/js/Group1.js
+++ src/Admin/js/Group1.js
@@ -126,43 +126,40 @@ export default class Group1 extends React.Component {
       "lesson_duration":document.querySelector('#formBasicTel1').value,
       "min_students":document.querySelector('#formBasicNote1').value,
       "max_students":document.querySelector('#formBasicNote2').value,
-      "description_file":document.querySelector('#passport_address').value,
-      "title_image":document.querySelector('#passport_number').value,
-      "cover_image":document.querySelector('#passport_serial').value,
+      // "description_file":document.querySelector('#passport_address').files,
+      // "title_image":document.querySelector('#passport_number').files,
+      // "cover_image":document.querySelector('#passport_serial').files,
       "deleted":document.querySelector('#passport_who_give').value,
       "notes":document.querySelector('#passport_when_give').value,
-      "course_section_id": false,
+      "course_section_id": null,
       "publicized": true,
       "curriculum": " ",
       "required_course_id": null,
       "video_link": null,
 },
-"start_date": "2018-07-20",
-"end_date": "2018-08-24",
-"classroom_building": "A",
-"classroom_room": "101",
+"start_date": document.querySelector('#date1').value,
+"end_date":document.querySelector('#date2').value,
+"classroom_building":document.querySelector('#school').value,
+"classroom_room": document.querySelector('#room').value,
 "finished": "2018-11-28 08:37:35",
+
 "timeslot": {
-    "id": 3,
-    "timeslot_name": "Odd days, evening times",
-    "start_time": "18:00:00",
-    "end_time": "20:00:00",
-    "duration": 120,
-    "mon": true,
-    "tue": false,
-    "wed": true,
-    "thu": false,
-    "fri": true,
-    "sat": false,
-    "sun": false,
+  
+  "timeslot_name":document.querySelector('#timeslot_name').value,
+    "start_time": document.querySelector('#start_time').value,
+    "end_time":document.querySelector('#end_time').value,
+    "duration": document.querySelector('#duration').value,
+    "mon":  document.querySelector('#mon').checked,
+    "tue": document.querySelector('#tue').checked,
+    "wed": document.querySelector('#wed').checked,
+    "thu": document.querySelector('#thu').checked,
+    "fri": document.querySelector('#fri').checked,
+    "sat": document.querySelector('#sat').checked,
+    "sun": document.querySelector('#sun').checked,
     "school_name": "Codecraft.uz",
     "branch_name": "Main branch"
 }
-
-
-
-    }
-    console.log(user)
+}
 axios.post('http://62.209.129.38:8000/api/groups/', user , {
 headers: {
   'Authorization': `Token ${access_token}` 
@@ -334,11 +331,11 @@ getGroupS(id).then(res=>{
   </Form.Group>
   <Form.Group className="mb-3" >
     <Form.Label>lessons <sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
-    <Form.Control type="email" className="mb-3" id="formBasicTel" placeholder="lessons" />
+    <Form.Control type="number" className="mb-3" id="formBasicTel" placeholder="lessons" />
   </Form.Group> 
    <Form.Group className="mb-3" >
     <Form.Label>lesson_duration</Form.Label>
-    <Form.Control type="email" className="mb-3" id="formBasicTel1" placeholder="lesson_duration" />
+    <Form.Control type="number" className="mb-3" id="formBasicTel1" placeholder="lesson_duration" />
   </Form.Group>
   <Form.Group className="mb-3" >
     <Form.Label>min_students</Form.Label>
@@ -356,25 +353,76 @@ getGroupS(id).then(res=>{
   </Form.Group>
   <Form.Group className="mb-3" >
     <Form.Label>title_image<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
-    <Form.Control type="email" id="passport_number" className="mb-3" placeholder="title_image" />
+    <Form.Control type="file" id="passport_number" className="mb-3" placeholder="title_image" />
   </Form.Group>
   <Form.Group className="mb-3" >
     <Form.Label>cover_image<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
-    <Form.Control type="email" id="passport_serial" className="mb-3" placeholder="cover_image" />
+    <Form.Control type="file" id="passport_serial" className="mb-3" placeholder="cover_image" />
   </Form.Group>
   <Form.Group className="mb-3" >
-    <Form.Label>deleted<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
-    <Form.Control type="email" id="passport_who_give" className="mb-3" placeholder="deleted" />
+  <Form.Label>Delete<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="number"  id="passport_who_give" className="mb-3" placeholder="deleted" />
   </Form.Group>
   <Form.Group className="mb-3" >
     <Form.Label>notes<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
-    <Form.Control type="email" id="passport_when_give" className="mb-3" placeholder="notes" />
+    <Form.Control type="text" id="passport_when_give" className="mb-3" placeholder="notes" />
   </Form.Group>
   <Form.Group className="mb-3" >
     <Form.Label>course_section_id<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
-    <Form.Control type="file" id="passport_file" className="mb-3" placeholder="course_section_id" />
+    <Form.Control type="number" id="passport_file" className="mb-3" placeholder="course_section_id" />
   </Form.Group>
-
+  <Form.Group className="mb-3" >
+    <Form.Label>start_date<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="date" id="date1" className="mb-3" placeholder="course_section_id" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Label>end_date<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="date" id="date2" className="mb-3" placeholder="course_section_id" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Label>classroom_building<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="text" id="school" className="mb-3" placeholder="course_section_id" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Label>classroom_room<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="text" id="room" className="mb-3" placeholder="course_section_id" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Label>timeslot_name<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="text" id="timeslot_name" className="mb-3" placeholder="timeslot_name" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Label>start_time<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="time" id="start_time" className="mb-3" placeholder="start_time" />
+  </Form.Group>  <Form.Group className="mb-3" >
+    <Form.Label>end_time<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="time" id="end_time" className="mb-3" placeholder="end_time" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Label>duration<sup style={{color:'red',fontSize:'18px',position:'relative',top:'3px'}}>*</sup></Form.Label>
+    <Form.Control type="number" id="duration" className="mb-3" placeholder="duration" />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="mon" id="mon" className="mb-3"  />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="tue" id="tue" className="mb-3"  />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="wed" id="wed" className="mb-3"  />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="thu" id="thu" className="mb-3"  />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="fri" id="fri" className="mb-3"  />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="sat" id="sat" className="mb-3"  />
+  </Form.Group>
+  <Form.Group className="mb-3" >
+    <Form.Check type="checkbox" label="sun" id="sun" className="mb-3"  />
+  </Form.Group>
 </Form></Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.handleClose}>
