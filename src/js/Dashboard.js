60a72c817d89a0c5ae4578c914fc3dc6591cb558
abbas <86982819+abbassiriyus@users.noreply.github.com>
2022-04-03 10:32:34 +0500

    dd
    dd

--- src/js/Dashboard.js
+++ src/js/Dashboard.js
@@ -84,7 +84,7 @@ export default class Dashboard extends Component {
 
 
     </Col>
-    <Col  lg={4}  md={12}   sm={{order:'first'}} style={{display:'flex',justifyContent:'center',alignItems:'center'}}><img  width="100%"  src={bola} alt=' '/></Col>
+    <Col  lg={4}  md={12}   sm={{order:'first'}} id={s.img13} style={{display:'flex',justifyContent:'center',alignItems:'center'}}><img  width="100%"  src={bola} alt=' '/></Col>
     <Col lg={4}  md={12}>
     <div  className={s.kard}  >
       <div ><img className={s.rasn} alt=" " src={online} /></div>
